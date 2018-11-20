97b61f77307bc27d4359bde1cc2ec72a599b48e9
Farhan Haider <farhanhedr@gmail.com>
2018-11-20 17:45:55 +0500

    Initial commit

---
+++ src/test/java/com/technifai/serialmountain/SerialmountainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.technifai.serialmountain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerialmountainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
